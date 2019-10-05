78c177670f31026de49c4614e2dff1b9c27f01f3
Elizabeth <elizabeth@dawnvisions.com>
2019-10-05 07:53:27 -0500

    Backbone Classes added

---
+++ src/BubbleSort.java
@@ -0,0 +1,14 @@
+public class BubbleSort {
+
+    static void bubbleSort(long arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[i]
+                    long temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+}
